e37981d928e85e4e3f0dcaec8649465ebac7976b
rileyale001 <alexjtriley@gmail.com>
2021-07-22 01:03:03 -0700

    committing on mAIN

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,34 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license !== "No license") {
+    return `![Github License](https://img.shields.io/badge/license-${license}-blue)`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license !== "No license") {
+    return `\n* [License](#license)\n`;
+  }
+  return "";
+}
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license !== "No license") {
+    return ``;
+  }
+  return "";
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
---
+++ Develop/utils/index.js
@@ -0,0 +1,70 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const path = require('path');
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your projects title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the instructions for installation to your project?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What is your project used for?',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can people contribute to your project?',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'How can your project be tested?',
+      },
+    {
+      type: 'checkbox',
+      message: 'What license do you want for the project?',
+      name: 'license',
+      choices: ['GPL', 'Common Development and Distribution License', 'Mozilla Public License 2.0', 'Node', 'MIT license', 'No license'],
+    },
+    // github and email questions
+    
+  ]
+  
+      
+    //   else {
+          
+    //   }
+    // const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+
+    // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    //   err ? console.log(err) : console.log('Success!')
+    // );
+  
+
+// TODO: Create a function to write README file fs doc google
+function writeToFile(fileName, data) {
+
+}
+
+// TODO: Create a function to initialize app call inquirer 
+function init() {
+  inquirer.prompt(questions)
+}
+
+// Function call to initialize app
+init();
